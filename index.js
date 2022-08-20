c856c70daf194bbbc0c687db4d934a273c1640fc
Gaurav <gaurav.narayan.varma@gmail.com>
2022-08-20 15:21:13 -0400

    commit tree edc1450a6bc453269f513d85f8a61b52b888b267

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your code here
+
+// class keyword- creates class
+// class has a constructor method
+class Dog {
+    constructor(name, breed) {
+        this.name = name,
+        this.breed = breed
+    }
+}
+
+let bigFluffyDog1 = new Dog("Buzz", "greatPyrenees")
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food,
+        this.drink = drink
+    }
+}
+
+let breakfast1 = new Breakfast("Buzz", "greatPyrenees")
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad,
+        this.soup = soup,
+        this.drink = drink
+    }
+}
+
+// initialized dessert as private property/field with #
+class Dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad,
+        this.soup = soup,
+        this.entree = entree,
+        this.#dessert = dessert
+    }
+}
+
+// must set private property in class before constructor
+
+// method in JS have dots in front of them
+// for example, myArray.forEach is a array method
+// method works on the array name
